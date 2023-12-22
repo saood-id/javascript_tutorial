4fd2679110ed14e5ccaf7628d6c6ae6e086ea86d
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-22 16:03:24 +0000

    conversion of data type

---
+++ 01_basics/conversion_Operation.js
@@ -0,0 +1,20 @@
+let score="saood"
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof score);
+console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//null=>0 
+//undefined=>NaN
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false;"anything"=>true
+
+let anyNumber=33
+let stringNumber=String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
