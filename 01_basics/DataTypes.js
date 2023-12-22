286522cad26305bbf137d304c22bf9059a32a4f3
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-22 15:02:32 +0000

    container config done

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all as new version code
+//alert(3+3) we cant use alert in node ,we use it in browser
+console.log(8+10)
+let name="saood"
+let  age=24
+let done=true
+//number->0 to 2^53
+//string
+//bool
+//symbol->uniqueness
+//undefined
+//null stand alone value
+console.log(typeof age)
+console.log(typeof null)//null ->object
