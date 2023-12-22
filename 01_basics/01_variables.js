955d0e73aa42166d3043b32785b51b663f70987f
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-22 14:34:52 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId= 14483
+let accountEmail="saoood@google.com"
+var  accountPassword="18772"
+accountcity="delhi"
+console.table([accountId,accountEmail,accountPassword,accountcity])
+//never use var use let
+//let state; this is undefined
