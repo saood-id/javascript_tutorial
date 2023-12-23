544e22137a628d2bbced04b0a0fc27af30889b12
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-23 08:18:25 +0000

    datatype summary

--- 01_basics/DataTypes_summary.js
+++ 01_basics/DataTypes_summary.js
@@ -20,3 +20,22 @@ let myobj={
 const myFunction = function(){
     console.log("why so serious?");
 }
+
+// stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName="gullyCricket"
+
+let anothername = myYoutubeName
+anothername="BabyOver"
+console.log(myYoutubeName);
+console.log(anothername);
+
+
+let userOne={
+    email:" saoodi@gmail.com",
+    upi:"crack@ybl"
+}
+let userTwo=userOne;
+userTwo.email="user@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
