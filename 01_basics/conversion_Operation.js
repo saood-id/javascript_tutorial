8a3597a5e507906d579fd5b87450bff98b0967da
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-23 07:14:44 +0000

    converesion config done

--- 01_basics/conversion_Operation.js
+++ 01_basics/conversion_Operation.js
@@ -1,8 +1,8 @@
 let score="saood"
-console.log(typeof score);
+//console.log(typeof score);
 let valueInNumber=Number(score)
-console.log(typeof score);
-console.log(valueInNumber);
+//console.log(typeof score);
+//console.log(valueInNumber);
 //"33"=>33
 //"33abc"=>NaN
 //true=>1;false=>0
@@ -10,11 +10,32 @@ console.log(valueInNumber);
 //undefined=>NaN
 let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
 //1=>true;0=>false
 //""=>false;"anything"=>true
 
 let anyNumber=33
 let stringNumber=String(anyNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//***********************Operations****************
+
+let value=3
+let negValue =-value
+//console.log(negValue)
+//console.log(2+2);
+//console.log(2*2);
+
+let str1="hello"
+let str2 =" saood "
+let str3=str1+str2
+//console.log(str3);
+//console.log("1"+2);
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+//console.log(+true);
+
+let gameCounter=18
+gameCounter++;
+console.log(gameCounter);
