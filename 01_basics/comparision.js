5dc5dd0047cdafec5ac4ce55d79ad981899e8096
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-23 07:34:20 +0000

    comparision config done

---
+++ 01_basics/comparision.js
@@ -0,0 +1,15 @@
+//console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("1"<2);
+// console.log("02">1);
+
+// console.log(null>0)
+// console.log(null==0);
+// console.log(null>=0); avoid this type of conversion 
+
+console.log("2"==2);
+console.log("2"===2);
