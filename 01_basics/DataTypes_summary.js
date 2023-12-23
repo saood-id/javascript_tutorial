eb7e02255888be58e8936d7f0faa32a7511c9a22
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-23 07:56:50 +0000

    dataType summary

---
+++ 01_basics/DataTypes_summary.js
@@ -0,0 +1,22 @@
+//primitive 
+
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//javaScript is Statically Typed language
+const Id=Symbol('123')
+const anotherId=Symbol('123')
+//console.log(Id==anotherId);
+
+// Reference(Non primitive)
+
+// Array, Objects, Functions
+
+const myHeroes=["IronMan","Hulk","Captain","SpiderMan"];
+let myobj={
+    name:"saood",
+    age:24,
+}
+
+const myFunction = function(){
+    console.log("why so serious?");
+}
