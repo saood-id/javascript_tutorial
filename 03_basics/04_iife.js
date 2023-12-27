ce41d41f2a29e4d9934398b30e8515668bc4f052
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2023-12-27 20:39:23 +0000

    immediately invoked function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// (function one(){
+    //named IIFE 
+//     console.log("DB CONNECTED");
+// })()//this is iife
+
+(()=>{
+    console.log(`DB CONNECTED`);
+})()//this is iife
