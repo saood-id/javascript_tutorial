4f3cfee0fdc411a91e87d1fec47a34f27050ed12
Mohammed saood ahmed <92047645+saood-id@users.noreply.github.com>
2024-02-05 07:34:39 +0000

    promises and fetch

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -78,13 +78,13 @@ async function consumePromiseFive(){
 
 consumePromiseFive()
 
-fetch('https://api.github.com/users/hiteshchoudhary')
-.then((response)=>{
-    return response.json()
-})
-.then((data)=>{
-    console.log(data);
-})
-.catch((error)=>{
-    console.log(error);
-})
+// fetch('https://api.github.com/users/hiteshchoudhary')
+// .then((response)=>{
+//     return response.json()
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
